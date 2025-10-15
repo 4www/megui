bc2b07a6c20b03ea2baaec0007ea3d3e44b21da9
4www <4www@duck.com>
2025-10-15 18:39:17 +0200

    re-use trunk

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+mod app;
+mod artwork;
+mod config;
+
+use app::MeguiApp;
+
+fn main() -> Result<(), eframe::Error> {
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        let options = eframe::NativeOptions {
+            viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
+            ..Default::default()
+        };
+
+        eframe::run_native(
+            "megui",
+            options,
+            Box::new(|_cc| Ok(Box::new(MeguiApp::default()))),
+        )
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use eframe::wasm_bindgen::JsCast;
+
+        // Log to WebAssembly console
+        eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+        let web_options = eframe::WebOptions::default();
+
+        wasm_bindgen_futures::spawn_local(async {
+            let document = web_sys::window()
+                .expect("No window")
+                .document()
+                .expect("No document");
+
+            let canvas = document
+                .get_element_by_id("the_canvas_id")
+                .expect("Failed to find the_canvas_id")
+                .dyn_into::<web_sys::HtmlCanvasElement>()
+                .expect("the_canvas_id was not a HtmlCanvasElement");
+
+            eframe::WebRunner::new()
+                .start(
+                    canvas,
+                    web_options,
+                    Box::new(|_cc| Ok(Box::new(MeguiApp::default()))),
+                )
+                .await
+                .expect("failed to start eframe");
+        });
+
+        Ok(())
+    }
+}
